4b15d32cebc1e131f52e3dc9efaa6ffdb2a7d99c
Chillbok <kylejunho@gmail.com>
2026-06-18 01:06:09 +0900

    feat(main.cpp): 첫 코드 추가
    ConsoleUtil.h 헤더파일에는 사용자의 입력 및 프로그램의 출력과 관련된 모든 작업들을 저장하도록 한다.
    일단 제목을 표시하는 Opening(), 구분선을 반환하는 get_divider() 두가지
    함수를 구현했음.

---
+++ include/ConsoleUtil.h
@@ -0,0 +1,14 @@
+/*
+이 파일에는 사용자의 입출력을 담당하는 함수들을 저장한다.
+*/
+#ifndef CONSOLE_UTIL_H
+#define CONSOLE_UTIL_H
+
+#include <string>
+
+void Opening();
+
+// 구분선을 반환하는 함수
+std::string get_divider();
+
+#endif
---
+++ src/ConsoleUtil.cpp
@@ -0,0 +1,14 @@
+#include "../include/ConsoleUtil.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::string;
+
+void Opening() {
+	cout << "이준호의 쇼핑몰" << endl;
+}
+
+string get_divider() {
+	return "===================================";
+}
