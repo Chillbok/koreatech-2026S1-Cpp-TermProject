bd46ef39a2c14e4ca7264e555dedbd8397b814f2
Chillbok <kylejunho@gmail.com>
2026-06-18 18:46:53 +0900

    feat(Account): 유저 계정 생성 기능 구현함
    set_user_information() 함수에서는 유저의 아이디와 비밀번호를 설정함.

---
+++ include/Account.h
@@ -0,0 +1,22 @@
+/*
+이 파일에는 사용자의 입출력을 담당하는 함수들을 저장한다.
+*/
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <string>
+
+class Account {
+private:
+	std::string id;
+	std::string password;
+public:
+	// 생성자
+	Account();
+	Account(std::string _id, std::string _password);
+	
+	// 함수
+	void set_user_information();
+};
+
+#endif
