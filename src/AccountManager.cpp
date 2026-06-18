bd9342b051c7c609557e9243491ec129a8f6db6a
Chillbok <kylejunho@gmail.com>
2026-06-18 22:08:25 +0900

    feat(AccountManager): 변수를 추가하는 과정을 구현했으나, 논리적 문제가 하나 발생함
    AccountManager 클래스와 Account 클래스의 목적에 따르면, 계정이 이미 존재하는지 확인하는 함수는 AccountManager에 하는 것이 맞음.
    그러나 현재는 아이디 생성을 Account 클래스에서 담당하고 있음.
    
    따라서 아이디 관리는 Account에서 하지만, 첫 생성만큼은 AccountManager에서 하는 게 맞다고 봄.
    단, Account 클래스의 생성자를 응용하는 것이 맞다고 생각함.

--- src/AccountManager.cpp
+++ src/AccountManager.cpp
@@ -5,6 +5,10 @@ using std::string;
 AccountManager::AccountManager() : account_list() {}
 
 void AccountManager::add_account_on_list() {
+	account_list.emplace_back();
+	// 아이디 생성 후 이미 존재하는 아이디인지 점검하는 과정은 여기에 구현하는 게 맞지 않을까?
+	// 그럼 생성을 여기에서 관리하는 게 맞다고 본다. 매개변수를 제공해야 하는 Account 생성자를 사용해서 수정하고 말이다.
+	account_list.back().set_user_information();
 }
 
 void AccountManager::remove_account_on_list(string user_id_for_delete) {
